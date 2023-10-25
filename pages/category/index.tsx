92c91f7c4bd819f91ecff2e5e07051272ecd7a48
HambarcumA <h.avagyannn@gmail.com>
2023-10-25 18:37:19 +0400

    first

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -1,8 +1,28 @@
+import { wrapper } from "@/app/store"
+import { getCategoriesThunk, getProductsThunk } from "@/features/product/productAPI"
+import { getCategorres, selectProduct } from "@/features/product/productSlice"
+import { useAppDispatch, useAppSelector } from "@/app/hooks"
+
 export default function ShowCategory() {
+  const {catArr}=useAppSelector(selectProduct)
+  const dispatch=useAppDispatch()
+
     return (
       <div>
         <h3>ShowCategory</h3>
+        <div className="form-control w-50" style={{border:'2px solid'}}>
+          {catArr.map((elm,i)=>
+            <h5 key={i}>{elm}</h5>
+          )}
+        </div>
       </div>
     )
   }
   
+
+  export const getStaticProps=wrapper.getStaticProps(
+    (store):any=>async()=>{
+        const data1=await store.dispatch(getCategoriesThunk()).unwrap()
+        store.dispatch(getCategorres(data1))
+    }
+  )
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,8 +1,26 @@
+import { useAppSelector } from "@/app/hooks"
+import { myAxios, wrapper } from "@/app/store"
+import { getProductByIdThunk } from "@/features/product/productAPI"
+import { getProductById, Product, selectProduct } from "@/features/product/productSlice"
+
+
 export default function SeeProduct() {
+  const {prod} = useAppSelector(selectProduct)
+
+  console.log(prod);
+  
     return (
       <div>
         <h3>SeeProduct</h3>
+        <h4>{prod.title}</h4>
+        <p>{prod.category}</p>
       </div>
     )
   }
-  
+
+export const getServerSideProps=wrapper.getServerSideProps(
+  (store):any=>async({params}:any)=>{
+  const product=await store.dispatch(getProductByIdThunk(+params.id)).unwrap()
+  store.dispatch(getProductById(product))
+  }
+)
